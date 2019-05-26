bbc7350d8699d5eedcb593d4b6cd3cba52ba4a71
Amit <shwartzamit17@gmail.com>
2019-05-26 12:08:31 +0300

    Access-Control-Allow-Credentials

--- service.js
+++ service.js
@@ -24,9 +24,8 @@ function startService(){
   app.use(bodyParser.raw());
 
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
   });
